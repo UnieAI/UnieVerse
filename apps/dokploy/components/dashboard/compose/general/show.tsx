c9916f38955a383f1811fad0321eeb83e08ce543
roy-shih <royshih2@gmail.com>
2025-05-12 22:09:42 +0800

    Update show.tsx

--- apps/dokploy/components/dashboard/compose/general/show.tsx
+++ apps/dokploy/components/dashboard/compose/general/show.tsx
@@ -82,8 +82,8 @@ export const ShowGeneralCompose = ({ composeId }: Props) => {
 
 	// console.log(data, data?.domains[0]?.host, data?.deployments?.[data.deployments.length - 1]?.deploymentId)
 
-	// const site = `http${data?.domains[0]?.https && 's'}://${data?.domains[0]?.host}`
-	const site = "http://example-server.uv-moore.test.unieai.com"
+	const site = `http${data?.domains[0]?.https && 's'}://${data?.domains[0]?.host}`
+	// const site = "http://example-server.uv-moore.test.unieai.com"
 	const screenshotUrl = `/api/screenshot?url=${encodeURIComponent(site)}`;
 
 	useEffect(() => {
