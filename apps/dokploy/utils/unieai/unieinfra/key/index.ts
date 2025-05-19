5e2544123b29bb5db80780f11210cf397ad732a6
ZoneTwelve <zonetwelve159@gmail.com>
2025-05-19 04:52:00 +0000

    Fix UnieInfra URL details

--- apps/dokploy/utils/unieai/unieinfra/key/index.ts
+++ apps/dokploy/utils/unieai/unieinfra/key/index.ts
@@ -4,9 +4,9 @@ export const isDevelopment: boolean = (process.env.NODE_ENV === "development");
 // the key to get UnieInfra access_token value from localStorage 
 export const ACCESS_TOKEN_KEY = "UnieInfraAccessToken";
 
-export const UNIEINFRA_OPENAI_API_URL = `${process.env.NEXT_PUBLIC_UNIEINFRA_API_URL}/v1`;
+export const UNIEINFRA_OPENAI_API_URL = `${process.env.NEXT_PUBLIC_UNIEINFRA_API_URL || process.env.UNIEINFRA_API_URL}/v1`;
 
-export const UNIEINFRA_SYSTEM_API_URL = `${process.env.NEXT_PUBLIC_UNIEINFRA_API_URL}/api`;
+export const UNIEINFRA_SYSTEM_API_URL = `${process.env.NEXT_PUBLIC_UNIEINFRA_API_URL || process.env.UNIEINFRA_API_URL}/api`;
 
 // ai-playground tab
 export const AI_PLAYGROUND_TAB_VALUE = {
