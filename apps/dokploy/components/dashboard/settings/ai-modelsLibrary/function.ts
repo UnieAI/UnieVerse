f3230a822510beec75141c1495a06e00f4b9d686
sw9487 <chouwei88929@gmail.com>
2025-05-27 22:53:43 +0800

    新增資料

--- apps/dokploy/components/dashboard/settings/ai-modelsLibrary/function.ts
+++ apps/dokploy/components/dashboard/settings/ai-modelsLibrary/function.ts
@@ -1,8 +1,8 @@
-import { ModelData_Payload,marker } from ".";
+import { ModelData_Payload, marker } from ".";
 
 export const fetchModelDatas = async (): Promise<ModelData_Payload[]> => {
 
-    const aaa:ModelData_Payload[] = [
+    const aaa: ModelData_Payload[] = [
         {
             "name": "aqua-mini",
             "img_src": "https://unie-backstage.unieai.com/api/mongodb/projects/image/get_image_by_id?image_id=189a6303-4c3e-437d-ad9a-bcb806a0efbc",
@@ -29,8 +29,26 @@ export const fetchModelDatas = async (): Promise<ModelData_Payload[]> => {
             "marker": marker.Text,
             "cost": "Input price:    $0.000/ 1K\nOutput price: $0.000/ 1K",
             "tokens": "40.5M"
+        },
+        {
+            "name": "Qwen/Qwen3-235B-A22B",
+            "img_src": "https://unie-backstage.unieai.com/api/mongodb/projects/image/get_image_by_id?image_id=44677346-ae62-48e0-877d-72eb1567e9b8",
+            "provider": "Qwen",
+            "description": "Qwen3-235B-A22B is a state-of-the-art large language model developed by Alibaba's Qwen team, featuring 235 billion parameters with a Mixture-of-Experts (MoE) architecture. It is instruction-tuned for multi-turn dialogue, reasoning, and multilingual understanding, achieving performance comparable to GPT-4 and Claude 3. The model supports a context window up to 128K tokens.",
+            "marker": marker.Text,
+            "cost": "Input price:    $0.020/ 1K  \nOutput price:   $0.040/ 1K",
+            "tokens": "128K"
+        },
+        {
+            "name": "meta-llama/Llama-4-Scout-17B-16E-Instruct",
+            "img_src": "https://unie-backstage.unieai.com/api/mongodb/projects/image/get_image_by_id?image_id=54c6070f-54a6-4f38-9b06-92f20dbd5566",
+            "provider": "meta-llama",
+            "description": "Llama-4-Scout-17B-16E-Instruct is a natively multimodal large language model developed by Meta, featuring a mixture-of-experts architecture with 17 billion activated parameters and 16 experts (109B total). It supports a context window of 128K tokens and is instruction-tuned for assistant-like chat, visual reasoning, and multilingual tasks across 12 languages. The model excels in text and image understanding, code generation, and multi-turn dialogue, with a knowledge cutoff of August 2024.",
+            "marker": marker.Text,
+            "cost": "Input price:    $0.080/ 1K\nOutput price:   $0.300/ 1K",
+            "tokens": "128K"
         }
-    ]
+    ];
 
     return aaa;
 
